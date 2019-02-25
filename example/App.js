d7e75ad4c2801ce8f67d427fb995ee144be49ec5
FreakyCoder <kurayogun@gmail.com>
2019-02-25 13:18:36 +0300

    Fixed PropTypes bug

--- example/App.js
+++ example/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Card from "@paraboly/react-native-card";
-import PropTypes from "prop-types";
 
 export default class App extends React.Component {
   render() {
